10ccdec41855f0dc4ea72d9ea5e2c1a325fa743a
fedemagri <fmagri@itba.edu.ar>
2024-10-27 00:00:27 -0300

    added qr3 as /

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Qr1 from '@/components/qr1.vue'
+import Qr3 from '@/components/qr3.vue'
 import Tickets from '@/components/tickets.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 
 const routes = [
   { path: '/tickets', name: 'Tickets', component: Tickets },
-  { path: '/qr1', name: 'Qr1', component: Qr1 }
+  { path: '/qr1', name: 'Qr1', component: Qr1 },
+  { path: '/', name: 'Qr3', component: Qr3 }
 ]
 
 const router = createRouter({
